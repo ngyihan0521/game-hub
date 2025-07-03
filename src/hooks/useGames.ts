1ccc2952060f8f337e837d46920bc981ac51b668
Addison Ng Yi Han <addison.ng0521@gmail.com>
2025-07-04 01:53:00 +0800

    Show loading skeleton

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -24,21 +24,28 @@ interface FetchGameResponse {
 const useGames = () => {
     const [games, setGames] = useState<Game[]>([]);
       const [error, setError] = useState("");
+      const [isLoading, setLoading] = useState(false);
     
       useEffect(() => {
         const controller = new AbortController();
 
+        setLoading(true);
         apiClient
           .get<FetchGameResponse>("/games", {signal: controller.signal})
-          .then((response) => setGames(response.data.results))
+          .then((response) => {
+            setGames(response.data.results);
+            setLoading(false);
+          })
           .catch((error) => {
             if (error instanceof CanceledError) return;
-            setError(error.message)});
+            setError(error.message);
+            setLoading(false);
+          });
 
           return () => controller.abort();
       }, []);
 
-      return {games, error}
+      return {games, error, isLoading}
 }
 
-export default useGames
+export default useGames;
